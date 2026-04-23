1b8cda278833d172e6eff05ea209455c9aa01918
Jesus Maria Vargas Logreira <134142244+JesusVargasLogreira@users.noreply.github.com>
2026-04-23 19:32:34 +0000

    Filtro busqueda

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -11,7 +11,6 @@ interface Digimon {
 type FiltroTipo = 'nombre' | 'nivel'
 
 function Home() {
-
     const [lista, setLista] = useState<Digimon[]>([])
 
     //filtro
@@ -19,7 +18,10 @@ function Home() {
 
     const filtros: FiltroTipo[] = ['nombre', 'nivel']
 
+    const [busqueda, setBusqueda] = useState('')
+
     useEffect(() => {
+        setBusqueda('')
         const fetchData = async () => {
             try {
                 const res = await fetch("https://digimon-api.vercel.app/api/digimon")
@@ -31,47 +33,104 @@ function Home() {
         }
 
         fetchData()
-    }, [])
+    }, [filtro])
 
-    const listaOrdenada =
-        filtro === 'nombre'
-            ? [...lista].sort((a, b) => a.name.localeCompare(b.name))
-            : [...lista].sort((a, b) => a.level.localeCompare(b.level))
+    const ordenNombre = lista.filter((digimon) =>
+        busqueda.length < 3
+            ? true // muestra todos si hay menos de 3 caracteres
+            : digimon.name.toLowerCase().includes(busqueda.toLowerCase())
+    ).sort((a, b) => a.name.localeCompare(b.name))
+
+    const ordenNivel = lista.filter((digimon) =>
+        busqueda.length < 3
+            ? true // muestra todos si hay menos de 3 caracteres
+            : digimon.name.toLowerCase().includes(busqueda.toLowerCase())
+    ).sort((a, b) => a.level.localeCompare(b.level))
+
 
     return (
         <>
             <div className="filtros">
-                {filtros.map((item) => (
+                {filtros.map((onestat) => (
                     <button
-                        key={item}
-                        onClick={() => setFiltro(item)}
+                        key={onestat}
+                        onClick={() => setFiltro(onestat)}
+                        className={filtro === onestat ? 'activo' : ''}
                     >
-                        {item}
+                        {onestat}
                     </button>
                 ))}
             </div>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Imagen</th>
-                        <th>Nivel</th>
-                    </tr>
-                </thead>
+            <input
+                type="text"
+                placeholder='Buscar...'
+                value={busqueda}
+                onChange={(e) => setBusqueda(e.target.value)}
+            />
 
-                <tbody>
-                    {listaOrdenada.map((digimon) => (
-                        <tr key={digimon.name}>
-                            <td>{digimon.name}</td>
-                            <td>
-                                <img src={digimon.img} width="80" />
-                            </td>
-                            <td>{digimon.level}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <div className="tabla-container">
+                <h2>Digimon</h2>
+                {filtro === 'nombre' ? (
+                    <table className="tabla-posiciones">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Imagen</th>
+                                <th>Nivel</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {ordenNombre.map((digimon) => (
+                                <tr key={digimon.name}
+                                    className={busqueda.length >= 3 &&
+                                        digimon.name.toLowerCase().includes(busqueda.toLowerCase())
+                                        ? 'resaltado'
+                                        : ''
+                                    }
+
+                                >
+                                    <td>{digimon.name}</td>
+                                    <td>
+                                        <img src={digimon.img} width="80" />
+                                    </td>
+                                    <td>{digimon.level}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                ) : (
+                    <table className="tabla-posiciones">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Imagen</th>
+                                <th>Nivel</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {ordenNivel.map((digimon) => (
+                                <tr key={digimon.name}
+                                    className={busqueda.length >= 3 &&
+                                        digimon.name.toLowerCase().includes(busqueda.toLowerCase())
+                                        ? 'resaltado'
+                                        : ''
+                                    }
+                                >
+                                    <td>{digimon.name}</td>
+                                    <td>
+                                        <img src={digimon.img} width="80" />
+                                    </td>
+                                    <td>{digimon.level}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )
+                }
+
+            </div>
+
         </>
     )
 }
